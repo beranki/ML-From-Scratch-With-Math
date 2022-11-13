2ad0873ec6fd6ff7608a08b570b1f09f655aff9a
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2022-11-12 22:36:19 -0800

    multlinearregress

--- regression/multi-linear-regression/MultiLinearRegression.py
+++ regression/multi-linear-regression/MultiLinearRegression.py
@@ -1,6 +1,26 @@
+import numpy as np
+
 class MultiLinearRegression:
-    def __init__(self, lr, epochs, W = None, b = 0):  
-        self.lr = lr
+    def __init__(self, alpha, epochs, W = None, b = 0):  
+        self.alpha = alpha
         self.epochs = epochs
         self.W = W
         self.b = b
+
+    def fit(self, X, y):
+        n = X.shape[0] #number of samples
+        m = len(y)
+        self.W = np.zeros(X.shape[1]).reshape(-1, 1) #initializing weights to same size as X
+
+        for epoch in range(self.epochs): #repeat gradient descent till convergence
+            y_pred = np.dot(X, self.W) + self.b
+            dW = 1/m * np.dot(X.T, (y_pred - y)) #computing the gradient of W
+            db = 1/m * np.sum(y_pred - y)
+
+            self.W -= self.alpha * dW #adjusting the weights
+            self.b -= self.alpha * db #adjusting the biases
+
+        return self.W, self.b
+
+    def predict(self, X):
+        return np.dot(X, self.W) + self.b
--- regression/multi-linear-regression/test.py
+++ regression/multi-linear-regression/test.py
@@ -2,22 +2,34 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from MultiLinearRegression import MultiLinearRegression
 
 def main():
     dataset = pd.read_csv("estates.csv")
-    X = np.array(dataset["X2 house age"]).reshape(-1,1)
-    y = np.array(dataset["Y house price of unit area"]).reshape(-1,1)
-    print(X.shape)
+    y = np.array(dataset["Y house price of unit area"]).reshape(-1, 1)
+    X = np.array(dataset.drop("Y house price of unit area", axis = 1))
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8,test_size=0.2,random_state=100)
-    mlr = MultiLinearRegression(0.01, 1000)
-    b_0, b_1 = mlr.fit(X_train, y_train)
+    s = StandardScaler()
+    X = s.fit_transform(X)
 
-    plt.scatter(X, y)
-    y_pred = b_0 + b_1*X_test
-    plt.plot(X_test, y_pred, color = "m")
-    plt.show()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8,test_size=0.2,random_state=42)
+
+    mlr = MultiLinearRegression(0.01, 1000) #learning rate of 0.01, 1000 epochs
+    W, b = mlr.fit(np.array(X_train), y_train)
+    y_pred = mlr.predict(X_test)
+
+    r2acc(y_pred, y_test) #runs w/ an r2 accuracy of .6777689831387611 on this dataset
+
+    print(W)
+    print(b)
+
+def r2acc(y_pred, y_test):
+    rss = np.sum((y_pred - y_test) ** 2)
+    tss = np.sum((y_test-y_test.mean()) ** 2)
+    
+    r2 = 1 - (rss / tss)
+    print(f"r2 accuracy: {r2}") 
 
 if __name__ == "__main__":
     main()
